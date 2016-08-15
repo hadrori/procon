9a3854a4fa929cf569853e37a516e0c4998537bc
hadrori <hadrori.hs@gmail.com>
2016-08-15 21:46:59 +0900

    Add moj for topcoder

---
+++ SRM/moj_4.18/template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+//#define int long long // do not forget "LL"
+
+class $CLASSNAME$ {
+public:
+        $RC$ $METHODNAME$( $METHODPARMS$ ) {
+                
+        }
+};
+//#undef int
+
+$BEGINCUT$
+$TESTCODE$
+
+$DEFAULTMAIN$
+$ENDCUT$
