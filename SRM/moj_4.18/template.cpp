2b8b8995dbe592eda4efe1aca0fed66961f42afd
hadrori <hadrori.hs@gmail.com>
2016-08-15 21:56:22 +0900

    Update SRM template

--- SRM/moj_4.18/template.cpp
+++ SRM/moj_4.18/template.cpp
@@ -1,12 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 //#define int long long // do not forget "LL"
+#define repi(i,a,b) for(int i=(int)(a);i<(int)(b);i++)
+#define rep(i,n) repi(i,0,n)
 
 class $CLASSNAME$ {
 public:
-        $RC$ $METHODNAME$( $METHODPARMS$ ) {
-                
-        }
+    $RC$ $METHODNAME$( $METHODPARMS$ ) {
+    }
 };
 //#undef int
 
